3715dc7a51898e764e00f421f2163d2704cbb9e8
Fabio Spampinato <spampinabio@gmail.com>
2024-03-02 18:42:09 +0000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,27 @@
+# Open DevTools
+
+<p align="center">
+  <img src="https://raw.githubusercontent.com/fabiospampinato/vscode-open-devtools/master/resources/logo.png" width="128" alt="Logo">
+</p>
+
+A simple extension for opening DevTools windows listed under `chrome://inspect`.
+
+## Install
+
+Follow the instructions in the [Marketplace](https://marketplace.visualstudio.com/items?itemName=fabiospampinato.vscode-open-devtools), or run the following in the command palette:
+
+```sh
+ext install fabiospampinato.vscode-open-devtools
+```
+
+## Usage
+
+It adds 1 command to the command palette:
+
+```js
+DevTools: Open // Open the last DevTools window listed under chrome://inspect
+```
+
+## License
+
+MIT © Fabio Spampinato
---
+++ src/commands.ts
@@ -0,0 +1,42 @@
+
+/* IMPORT */
+
+import * as openPath from 'open';
+import * as vscode from 'vscode';
+
+/* MAIN */
+
+const open = async (): Promise<void> => {
+
+  for ( const port of [9222, 9229, 5858] ) {
+
+    try {
+
+      const response = await globalThis.fetch ( `http://127.0.0.1:${port}/json/list` );
+
+      if ( !response.ok ) continue;
+
+      const devtools = await response.json ();
+      const devtoolUrl: string = devtools[devtools.length - 1]?.devtoolsFrontendUrl; // The last one is the most recent one, presumably
+
+      if ( !devtoolUrl ) continue;
+
+      await openPath ( devtoolUrl, { app: { name: openPath.apps.chrome }, newInstance: true } );
+
+      return;
+
+    } catch ( error ) {
+
+      // console.log ( error );
+
+    }
+
+    vscode.window.showErrorMessage ( 'No DevTools windows found' );
+
+  }
+
+}
+
+/* EXPORT */
+
+export {open};
